fcf9554112de370314526be54f8a7ddb73da5c00
Morty <51201738+kangeli13@users.noreply.github.com>
2024-03-14 12:51:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(x/12);
 // Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
 //  Verify that name1 is different from name2 (HINT: check their equality to be false).
 //  Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
-let name1 = "john"; 
+let name1 = "john";  
     
 let name2 = "John";    
       
