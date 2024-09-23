84935e8055f511aeace8cd8e4a487862aa4f32a0
Morty <51201738+kangeli13@users.noreply.github.com>
2024-09-23 15:49:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Create a variable named x and assign to it the number 14. 
 // Execute a subtraction between the number 12 and the variable x.
-let x = 14;        
+let x = 14;         
 console.log(x/12);                  
               
 // Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
