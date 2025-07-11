20b80c2d25a3e7f75103b44a5ae05fb09e8055d0
Morty <51201738+kangeli13@users.noreply.github.com>
2025-07-11 15:35:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Execute a subtraction between the number 12 and the variable x.
 let x = 14;           
 console.log(x/12);                      
-                 
+                  
 // Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
 //  Verify that name1 is different from name2 (HINT: check their equality to be false).
 //  Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
