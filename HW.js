9366ef2b8b2667ec283d5b0219654e4aae3be2af
Morty <51201738+kangeli13@users.noreply.github.com>
2025-09-08 17:57:03 +0100

    Update HW.js

--- HW.js
+++ HW.js
@@ -1,6 +1,6 @@
 
 //  Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
- 
+  
   
 const area = function (l1, l2){     
   return l1*l2;   
